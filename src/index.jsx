22ee7d4fb0ef87c5111c597e592347ddd8484ee2
Asis Pattisahusiwa <pattisahusiwa@users.noreply.github.com>
2020-10-02 03:03:14 +0900

    Silent bootstrap deprecation warning

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import DemoApp from './demo/DemoApp';
-import 'bootstrap/scss/bootstrap.scss';
-import 'admin-lte/build/scss/AdminLTE.scss';
+import './main.scss';
 import 'admin-lte/build/js/AdminLTE';
 
 ReactDOM.render(
