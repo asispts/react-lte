725a2645b733cad485cde1f74a79d882737c76f2
Asis Pattisahusiwa <pattisahusiwa@users.noreply.github.com>
2020-10-25 19:24:28 +0900

    Add DemoApp unit test

---
+++ src/demo/__tests__/DemoApp.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import DemoApp from '../DemoApp';
+
+test('Should render without crash', () => {
+  const dom = <DemoApp />;
+  const { container } = render(dom, { wrapper: MemoryRouter });
+
+  expect(container.firstChild).not.toBeNull();
+});
