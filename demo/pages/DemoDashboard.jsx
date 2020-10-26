d84728ef9fca7337ab221a8f50203ab1c2e46bf8
Asis Pattisahusiwa <pattisahusiwa@users.noreply.github.com>
2020-10-26 12:05:28 +0900

    Add table to dashboard

--- demo/pages/DemoDashboard.jsx
+++ demo/pages/DemoDashboard.jsx
@@ -3,15 +3,18 @@ import {
   faChartPie,
   faCloudDownloadAlt,
   faCog,
+  faMinus,
   faShoppingBasket,
   faShoppingCart,
   faTag,
   faThumbsUp,
+  faTimes,
   faUserPlus,
   faUsers,
 } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-import { Col, Row } from 'reactstrap';
+import { Badge, Card, CardBody, CardHeader, CardTitle, Col, Row, Table } from 'reactstrap';
 import LteContent from '../../src/components/LteContent';
 import LteContentHeader from '../../src/components/LteContentHeader';
 import LteInfoBox from '../../src/components/LteInfoBox';
@@ -54,7 +57,134 @@ export default function DemoDashboard() {
         </Row>
 
         <Row>
-          <Col lg='8' />
+          <Col lg='8'>
+            <Card>
+              <CardHeader className='border-transparent'>
+                <CardTitle>Latest Orders</CardTitle>
+                <div className='card-tools'>
+                  <button type='button' className='btn btn-tool' data-card-widget='collapse'>
+                    <FontAwesomeIcon icon={faMinus} />
+                  </button>
+                  <button type='button' className='btn btn-tool' data-card-widget='remove'>
+                    <FontAwesomeIcon icon={faTimes} />
+                  </button>
+                </div>
+              </CardHeader>
+              <CardBody className='p-0'>
+                <Table responsive>
+                  <thead>
+                    <tr>
+                      <th>Order ID</th>
+                      <th>Item</th>
+                      <th>Status</th>
+                      <th>Popularity</th>
+                    </tr>
+                  </thead>
+
+                  <tbody>
+                    <tr>
+                      <td>OR9842</td>
+                      <td>Call of Duty IV</td>
+                      <td>
+                        <Badge tag='span' color='success'>
+                          Shipped
+                        </Badge>
+                      </td>
+                      <td>
+                        <div className='sparkbar' data-color='#00a65a' data-height='20'>
+                          90,80,90,-70,61,-83,63
+                        </div>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>OR1848</td>
+                      <td>Samsung Smart TV</td>
+                      <td>
+                        <Badge tag='span' color='warning'>
+                          Pending
+                        </Badge>
+                      </td>
+                      <td>
+                        <div className='sparkbar' data-color='#f39c12' data-height='20'>
+                          90,80,-90,70,61,-83,68
+                        </div>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>OR7429</td>
+                      <td>iPhone 6 Plus</td>
+                      <td>
+                        <Badge tag='span' color='danger'>
+                          Delivered
+                        </Badge>
+                      </td>
+                      <td>
+                        <div className='sparkbar' data-color='#f56954' data-height='20'>
+                          90,-80,90,70,-61,83,63
+                        </div>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>OR7429</td>
+                      <td>Samsung Smart TV</td>
+                      <td>
+                        <Badge tag='span' color='info'>
+                          Processing
+                        </Badge>
+                      </td>
+                      <td>
+                        <div className='sparkbar' data-color='#00c0ef' data-height='20'>
+                          90,80,-90,70,-61,83,63
+                        </div>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>OR1848</td>
+                      <td>Samsung Smart TV</td>
+                      <td>
+                        <Badge tag='span' color='warning'>
+                          Pending
+                        </Badge>
+                      </td>
+                      <td>
+                        <div className='sparkbar' data-color='#f39c12' data-height='20'>
+                          90,80,-90,70,61,-83,68
+                        </div>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>OR7429</td>
+                      <td>iPhone 6 Plus</td>
+                      <td>
+                        <Badge tag='span' color='danger'>
+                          Delivered
+                        </Badge>
+                      </td>
+                      <td>
+                        <div className='sparkbar' data-color='#f56954' data-height='20'>
+                          90,-80,90,70,-61,83,63
+                        </div>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>OR9842</td>
+                      <td>Call of Duty IV</td>
+                      <td>
+                        <Badge tag='span' color='success'>
+                          Shipped
+                        </Badge>
+                      </td>
+                      <td>
+                        <div className='sparkbar' data-color='#00a65a' data-height='20'>
+                          90,80,90,-70,61,-83,63
+                        </div>
+                      </td>
+                    </tr>
+                  </tbody>
+                </Table>
+              </CardBody>
+            </Card>
+          </Col>
           <Col lg='4'>
             <LteInfoBox icon={faTag} text='Inventory' number='5,200' bgColor='warning' />
             <LteInfoBox icon={faHeart} text='Mentions' number='92,050' bgColor='success' />
